89794d6c4b6ea3edd61f48cc3d279d11651f784a
devlulcas <lucasalvesrego.contato@gmail.com>
2023-06-05 23:54:39 -0300

    feat: target esm using vite-plugin-node

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,11 @@ import { PORT } from '@/config/envs';
 
 const server = createServer(app);
 
-server.listen(PORT, () => {
-	console.log(`Server listening on port ${PORT}`);
-});
+
+if (import.meta.env.PROD) {
+	server.listen(PORT, () => {
+		console.log(`Server listening on port ${PORT}`);
+	});
+}
+
+export const devApp = app;
---
+++ vite.config.ts
@@ -0,0 +1,22 @@
+import { defineConfig } from 'vite';
+import { VitePluginNode } from 'vite-plugin-node';
+
+export default defineConfig({
+	server: {
+		port: 3000
+	},
+	resolve: {
+		alias: {
+			'@': '/src',
+			'@database': '/src/database/index'
+		}
+	},
+	plugins: [
+		...VitePluginNode({
+			adapter: 'express',
+			appPath: './src/index.ts',
+			exportName: 'devApp',
+			tsCompiler: 'esbuild',
+		})
+	],
+});
